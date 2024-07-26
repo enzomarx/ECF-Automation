7e10a8e8ff2d0f0b20e1a2bb43bc4d337a0d6220
enzomarx <enzomarxsantos@gmail.com>
2024-07-26 14:26:14 -0300

    flask

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template_string
+from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def show_log():
@@ -33,13 +35,27 @@ def show_log():
     </head>
     <body>
         <h1>Log de Automação</h1>
-        <div class="log-container">
-            <pre>{{ log_content }}</pre>
+        <div class="log-container" id="log-container">
+            <pre id="log-content">{{ log_content }}</pre>
         </div>
+        <script src="https://cdn.socket.io/4.0.1/socket.io.min.js"></script>
+        <script>
+            var socket = io();
+            socket.on('log_update', function(data) {
+                var logContainer = document.getElementById('log-container');
+                var logContent = document.getElementById('log-content');
+                logContent.textContent += data.message + '\\n';
+                logContainer.scrollTop = logContainer.scrollHeight;
+            });
+        </script>
     </body>
     </html>
     """
     return render_template_string(html_template, log_content=log_content)
 
+@socketio.on('log_update')
+def handle_log_update(data):
+    emit('log_update', data, broadcast=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
