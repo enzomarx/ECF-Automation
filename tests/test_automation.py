bca7ce4d2450034dd9aab4d4cfd8ebc402ce79e2
enzomarx <enzomarxsantos@gmail.com>
2024-07-26 14:28:52 -0300

    test

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -3,8 +3,7 @@ from src.automation import enviar_ecf
 
 class TestAutomation(unittest.TestCase):
     def test_enviar_ecf(self):
-        # Aqui você pode adicionar testes para a função enviar_ecf
-        # Por exemplo, verificar se a função completa sem erros
+        # 
         try:
             enviar_ecf('12345678000195', '01/2023')
         except Exception as e:
